5bfb03d71a24655e7ecb13b3359a77669552595d
sankovi4 <105877559+sankovi4@users.noreply.github.com>
2025-05-17 21:53:25 +0300

    Add_Damage/Dsath/Weapns

---
+++ Assets/PC_Death.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using AK.Wwise;
+public class PC_Death : MonoBehaviour
+{
+    public AK.Wwise.Event PCDeathEvent;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
