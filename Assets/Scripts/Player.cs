1dc335954109e1dae99f7d8084072add61bdf0c0
Mopsik62 <74981499+Mopsik62@users.noreply.github.com>
2025-05-17 14:31:49 +0300

    Improved intro + added colliders

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,8 +7,15 @@ public class Player : MonoBehaviour
     private Animator animator;
     private Rigidbody2D rb;
     private Vector2 moveInput;
+    [SerializeField]
+    private Footsteps_Concrete _footsteps;
+    [SerializeField]
+    private float _footsteps_cooldown = 0.9f; //время аниматора для 1 шага
+    private float _footsteps_timer;
+
     void Start()
     {
+        _footsteps_timer = 0f;
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
     }
@@ -17,7 +24,31 @@ public class Player : MonoBehaviour
     {
         moveInput.x = Input.GetAxisRaw("Horizontal");
         moveInput.y = Input.GetAxisRaw("Vertical");
+        AnimateMovement();
+    }
 
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        rb.MovePosition(rb.position + moveInput * moveSpeed * Time.fixedDeltaTime);
+        if (moveInput != Vector2.zero)
+        {
+            _footsteps_timer += Time.fixedDeltaTime;
+            Debug.Log(_footsteps_timer);
+            if (_footsteps_timer >= _footsteps_cooldown)
+            {
+                _footsteps.footstepConcreteEvent.Post(gameObject);
+                _footsteps_timer = 0;
+                Debug.Log("dsad");
+            }
+        }
+    }
+    void AnimateMovement()
+    {
         if (moveInput != Vector2.zero)
         {
             transform.localScale = new Vector3(2, 2, 2);
@@ -43,10 +74,6 @@ public class Player : MonoBehaviour
         }
     }
 
-    void FixedUpdate()
-    {
-        rb.MovePosition(rb.position + moveInput * moveSpeed * Time.fixedDeltaTime);
-    }
 
 
 }
